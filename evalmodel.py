96865273af074f0046e0bb524b31ee6c6ecd26cc
Wilson Wongso <wilsonwong961@gmail.com>
2019-07-06 14:31:51 +0700

    Initial Commit

---
+++ evalmodel.py
@@ -0,0 +1,77 @@
+import tensorflow as tf
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import cv2
+from pathlib import Path
+from sklearn.metrics import confusion_matrix
+from mlxtend.plotting import plot_confusion_matrix
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+
+model = tf.keras.models.load_model('gpu_trained_model_local.h5')
+
+test_dir = Path('chest_xray/test')
+
+normal_cases_dir = test_dir / 'NORMAL'
+pneumonia_cases_dir = test_dir / 'PNEUMONIA'
+
+normal_cases = normal_cases_dir.glob('*.jpeg')
+pneumonia_cases = pneumonia_cases_dir.glob('*.jpeg')
+
+test_data = []
+test_labels = []
+
+for img in normal_cases:
+    img = cv2.imread(str(img))
+    img = cv2.resize(img, (128, 128))
+    if img.shape[2] == 1:
+        img = np.dstack([img, img, img])
+    else:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.astype(np.float32)/255.
+    label = tf.keras.utils.to_categorical(0, num_classes=2)
+    test_data.append(img)
+    test_labels.append(label)
+
+for img in pneumonia_cases:
+    img = cv2.imread(str(img))
+    img = cv2.resize(img, (128, 128))
+    if img.shape[2] == 1:
+        img = np.dstack([img, img, img])
+    else:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.astype(np.float32)/255.
+    label = tf.keras.utils.to_categorical(1, num_classes=2)
+    test_data.append(img)
+    test_labels.append(label)
+
+test_data = np.array(test_data)
+test_labels = np.array(test_labels)
+
+print("Total number of test examples: ", test_data.shape)
+print("Total number of labels:", test_labels.shape)
+
+# Get predictions
+preds = model.predict(test_data, batch_size=16)
+preds = np.argmax(preds, axis=-1)
+
+# Original labels
+orig_test_labels = np.argmax(test_labels, axis=-1)
+
+cm = confusion_matrix(orig_test_labels, preds)
+plt.figure()
+plot_confusion_matrix(cm, figsize=(12, 8), hide_ticks=True,
+                      cmap=plt.cm.Blues)
+plt.xticks(range(2), ['Normal', 'Pneumonia'], fontsize=16)
+plt.yticks(range(2), ['Normal', 'Pneumonia'], fontsize=16)
+plt.show()
+
+tn, fp, fn, tp = cm.ravel()
+
+precision = tp/(tp+fp)
+recall = tp/(tp+fn)
+
+print("Recall of the model is {:.2f}".format(recall))
+print("Precision of the model is {:.2f}".format(precision))
